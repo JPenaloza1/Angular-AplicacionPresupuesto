09e1a18975240e5484bf47b42bf61a970edf8eea
JPenaloza1 <100963782+JPenaloza1@users.noreply.github.com>
2022-10-19 21:11:05 -0500

    Apuntes 19/10/22

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { EgresoComponent } from './egreso/egreso.component';
+import { CabeceroComponent } from './cabecero/cabecero.component';
+import { IngresoServicio } from './ingreso/ingreso.service';
+import { EgresoServicio } from './egreso/egreso.service';
+import { FormularioComponent } from './formulario/formulario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IngresoComponent,
+    EgresoComponent,
+    CabeceroComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [IngresoServicio, EgresoServicio],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
